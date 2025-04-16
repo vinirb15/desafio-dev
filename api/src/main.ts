9bc0d8186aad4ce3493b4867f76a333acd8fe4ea
vbueno <viniciusbueno15@gmail.com>
2025-04-16 17:13:01 -0300

    (feat): Enable CORS and update port configuration
    - Enable CORS for the NestJS application
    - Change default port from 3000 to 5000
    - Update Swagger setup and configuration

--- api/src/main.ts
+++ api/src/main.ts
@@ -3,17 +3,15 @@ import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-
+  const app = await NestFactory.create(AppModule, { cors: true });
   const config = new DocumentBuilder()
     .setTitle('API Desafio Dev')
     .setDescription('API para upload e normalização de arquivos CNAB')
     .setVersion('1.0')
     .build();
-
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
 
-  await app.listen(process.env.PORT ?? 3000);
+  SwaggerModule.setup('api', app, document);
+  await app.listen(process.env.PORT ?? 5000);
 }
 bootstrap();
